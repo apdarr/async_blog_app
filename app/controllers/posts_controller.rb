e869e37be7064b1caf7ad879393a8b0f89f449f6
Alex Darr <adarr@adarr-ltm.internal.salesforce.com>
2016-03-30 09:42:48 -0400

    Deploying changes to heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,14 +26,13 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      sleep(5)
+
+      flash[:notice] = "Post was created"
+      redirect_to posts_path
+    else
+      render :new
     end
   end
 
