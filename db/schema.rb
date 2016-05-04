760c557c9c416994f67c72d255c26b4a4996278a
apdarr <alexdarr@gmail.com>
2016-05-04 14:55:48 -0400

    Add index to post name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329132621) do
+ActiveRecord::Schema.define(version: 20160504185104) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20160329132621) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "posts", ["name"], name: "index_posts_on_name"
 
 end
