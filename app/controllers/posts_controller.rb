301ded1bd6fc41b18c03fe8cd4be5b3337f538db
apdarr <alexdarr@gmail.com>
2016-06-10 10:38:29 -0400

    testing index POST

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    response.headers['X-Csrf-Token'] = form_authenticity_token
     @posts = Post.all
     puts "count#index.load=1"
   end
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    response.headers['X-Csrf-Token'] = form_authenticity_token
+    #response.headers['X-Csrf-Token'] = form_authenticity_token
     @post = Post.new
   end
 
